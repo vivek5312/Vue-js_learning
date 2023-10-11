88116da81830d743eee36f0b47eeb65641da6aa0
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-10-11 16:00:08 +0530

    methods

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Vivek Vue.js App"/>
+  <HomeView/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './components/HomeView.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Email {{ email }}</h1>
+  
+  <h1>Name: {{ getname("vivek") }}</h1>
+  <h1> Data: {{getData()}}</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      email: "vivek@gmail.com",
+    };
+  },
+  methods:{
+    getname(name){
+        return name
+    },
+    getData(){
+        return {
+            name:'vivek',
+            age:22,
+            email:this.email
+        }
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
